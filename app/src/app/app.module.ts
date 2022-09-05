ed4e9bbd10dd172723c4286b6b87464a72919d19
Jaime Gomez <gomezcos@gmail.com>
2022-09-05 00:43:43 -0600

    first commit

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule} from '@angular/material/input';
+import { HomeComponent } from './home/home.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule} from '@angular/material/snack-bar';
+import { DialogClienteComponent } from './cliente/dialog/dialogcliente.component';
+import { FormsModule } from '@angular/forms';
+import { DialogDeleteComponent } from './common/delete/dialogdelete.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ClienteComponent,
+    DialogClienteComponent,
+    DialogDeleteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatTableModule,
+    HttpClientModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    FormsModule
+  ],
+  providers: [MatDialog],
+  bootstrap: [AppComponent],
+  entryComponents:[DialogClienteComponent]
+})
+export class AppModule { }
